2005c36db726d639b0ad2b07f20c1a2096e90360
lewypopescu <popesculewy@gmail.com>
2024-05-22 16:22:42 +0200

    ok done good

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ const perPage = 40;
 
 form.addEventListener('submit', onSearch);
 
-async function onSearch(e) {
-  e.preventDefault();
-  searchQuery = e.currentTarget.elements.searchQuery.value.trim();
+async function onSearch(event) {
+  event.preventDefault();
+  searchQuery = event.currentTarget.elements.searchQuery.value.trim();
   page = 1;
   gallery.innerHTML = '';
 
@@ -45,18 +45,10 @@ async function fetchImages() {
 
     renderGallery(hits);
 
-    if (hits.length !== perPage) {
-      Notiflix.Notify.info(
-        "We're sorry, but you've reached the end of search results."
-      );
-    }
-
-    page += 1;
     const lightbox = new SimpleLightbox('.gallery a');
     lightbox.refresh();
   } catch (error) {
     console.error(error);
-    Notiflix.Notify.failure('Please try again later!');
   }
 }
 
@@ -90,10 +82,38 @@ function renderGallery(images) {
   gallery.insertAdjacentHTML('beforeend', markup);
 }
 
+async function fetchImagesScroll() {
+  const API_KEY = '43911097-2767f3575ad906659ba392cfc';
+  const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
+
+  try {
+    const response = await axios.get(URL);
+    const { hits, total, totalHits } = response.data;
+
+    renderGallery(hits);
+
+    page += 1;
+    const lightbox = new SimpleLightbox('.gallery a');
+    lightbox.refresh();
+
+    let pagesTotal = totalHits / perPage;
+
+    if (page >= pagesTotal) {
+      setTimeout(() => {
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results"
+        );
+      }, 1000);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 window.addEventListener('scroll', () => {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
   if (scrollTop + clientHeight >= scrollHeight - 5) {
-    fetchImages();
+    fetchImagesScroll();
   }
 });
